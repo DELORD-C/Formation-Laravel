063f31866bdfca951a53238ed79b494c419bd5d0
Clément DELORD <clement.delord@epitech.eu>
2023-03-01 12:03:06 +0100

    Mix & Bootstrap

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,8 @@
 <html>
 <head>
     <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
 </head>
 <body>
     <x-nav/>
@@ -24,5 +25,6 @@
         @endif
         @yield('content')
     </div>
+    <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
